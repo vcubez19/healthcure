0166a40067b5e49b6717e3a0bf9e697b8b5906f1
Vincent Cubit <vinniecubit@yahoo.com>
2024-07-16 13:07:01 -0500

    Created register page

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -13,6 +13,9 @@ import { Input } from "@/components/ui/input"
 import { Control } from "react-hook-form";
 import { FormFieldType } from "./forms/PatientForm";
 import Image from "next/image";
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+
 
 interface CustomProps {
   control: Control<any>,
@@ -58,6 +61,21 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
           </FormControl>
         </div>
       )
+
+      case FormFieldType.PHONE:
+        return (
+          <FormControl>
+            <PhoneInput
+              defaultCountry="US"
+              placeholder={placeholder}
+              international
+              withCountryCallingCode
+              value={field.value}
+              onChange={field.onChange}
+              className="input-phone"
+            />
+          </FormControl>
+        )
     
     default:
       break;
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -4,9 +4,13 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
-import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+
+import { useState } from "react";
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
 
 export enum FormFieldType {
     INPUT = "input",
@@ -18,33 +22,47 @@ export enum FormFieldType {
     SKELETON = "skeleton"
 }
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
-
 const PatientForm = () => {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: ""
     },
   })
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+        const userData = {
+            name,
+            email,
+            phone
+        }
+
+        // const user = await createUser(userData)
+
+        // if (user) {
+        //     router.push(`/patients/${user.$id}/register`)
+        // }
+    } catch (error) {
+        console.log(error)
+    }
   }
+
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <section className="mb-12 space-y-4">
             <h1 className="text-2xl">Hello</h1>
+            <p>Let's get you started</p>
         </section>
 
         <CustomFormField 
@@ -56,8 +74,28 @@ const PatientForm = () => {
             iconSrc="/assets/icons/user.svg"
             iconAlt="user"
         />
+
+        <CustomFormField 
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="johndoe@example.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+        />
+
+        <CustomFormField 
+            fieldType={FormFieldType.PHONE}
+            control={form.control}
+            name="phone"
+            label="Phone number"
+            placeholder="(555) 123-4567"
+        />
         
-        <Button type="submit">Submit</Button>
+        <SubmitButton isLoading={isLoading}>
+            Get started
+        </SubmitButton>
       </form>
     </Form>
   )
