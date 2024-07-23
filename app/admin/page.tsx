7b8d97a24ce197da9d49f63558e44ee91876c565
Vincent Cubit <vinniecubit@yahoo.com>
2024-07-23 17:09:49 -0500

    downloading appointments from server

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from "next/image";
+import StatCard from '@/components/StatCard';
+import { getRecentAppointmentsList } from '@/lib/actions/appointment.actions';
 
-const Admin = () => {
+const Admin = async () => {
+
+  const appointments = await getRecentAppointmentsList();
+
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
         <header className="admin-header">
@@ -25,9 +30,30 @@ const Admin = () => {
                 <p className="text-dark-700">Manage new appointments</p>
             </section>
 
-            <section>
-                
+            <section className="admin-stat">
+                <StatCard
+                    type="appointments"
+                    count={appointments.scheduledCount}
+                    label="Scheduled appointments"
+                    icon="/assets/icons/appointments.svg"
+                />
+
+                <StatCard
+                    type="pending"
+                    count={appointments.pendingCount}
+                    label="Pending appointments"
+                    icon="/assets/icons/pending.svg"
+                />
+
+                <StatCard
+                    type="cancelled"
+                    count={appointments.cancelledCount}
+                    label="Cancelled appointments"
+                    icon="/assets/icons/cancelled.svg"
+                />
             </section>
+
+            
         </main>
     </div>
   )
