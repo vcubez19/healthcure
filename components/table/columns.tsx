1e6b7f4f814f254ced4126a9369fd80978ae9394
Vincent Cubit <vinniecubit@yahoo.com>
2024-07-23 18:19:21 -0500

    Showing doctor names in data table rows

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -20,6 +20,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import StatusBadge from "../StatusBadge"
+import { formatDateTime } from "@/lib/utils"
+import { Doctors } from "@/constants"
+import Image from "next/image"
 
 export type Payment = {
   id: string
@@ -51,12 +54,34 @@ export const columns: ColumnDef<Payment>[] = [
     },
   },
   {
-    accessorKey: "email",
-    header: "Email",
+    accessorKey: "schedule",
+    header: "Appointment",
+    cell: ({ row }) => (
+      <p className="text-14-regular min-w-[100px]">
+        {formatDateTime(row.original.schedule).dateTime}
+      </p>
+    )
   },
   {
-    accessorKey: "amount",
-    header: "Amount",
+    accessorKey: "primaryPhysician",
+    header: "Doctor",
+    cell: ({ row }) => {
+      const doctor = Doctors.find((doc) => doc.name === row.original.primaryPhysician)
+
+      return <div className="flex items-center gap-3">
+        <Image 
+          src={doctor?.image}
+          alt={doctor?.name}
+          width={100}
+          height={100}
+          className="size-8"
+        />
+
+        <p className="whitespace-nowrap">
+          Dr. {doctor?.name}
+        </p>
+      </div>
+    }
   },
   {
     id: "actions",
