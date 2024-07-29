d0d2a22e3fb0ad52dea51dba179f7a8bb0bf3984
Vincent Cubit <vinniecubit@yahoo.com>
2024-07-29 14:07:16 -0500

    Removed unused imports

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from "next/image";
-import Link from 'next/link';
 import RegisterForm from '@/components/forms/RegisterForm';
 import { getUser } from '@/lib/actions/patient.actions';
 import * as Sentry from "@sentry/nextjs";
