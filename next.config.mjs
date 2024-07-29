a1b6dcf75e110fa71a7efb3e115f94ba1b289b77
Vincent Cubit <vinniecubit@yahoo.com>
2024-07-29 13:38:36 -0500

    disable typescript

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@ import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    }
 };
 
 export default withSentryConfig(nextConfig, {
