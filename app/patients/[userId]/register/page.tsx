859f049fad170889202f5a04f80da026504a43fd
Vincent Cubit <vinniecubit@yahoo.com>
2024-07-29 20:44:55 -0500

    Changed image size on patient registration page

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -14,13 +14,15 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container">
         <div className="sub-container max-w-full flex-1 flex-col py-10">
-          <Image
-            src="/assets/icons/logo-full.svg"
-            height={1000}
-            width={1000}
-            alt="logo"
-            className="mb-12 h-10 w-fit"
-          />
+           <div className="w-1/3 mb-10">
+              <Image
+                src="/assets/icons/logo-full.svg"
+                height={1000}
+                width={1000}
+                alt="patient"
+                layout="responsive"
+              />
+           </div>
 
           <RegisterForm user={user}/>
 
