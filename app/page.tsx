30dc4665d3f35498ca09c4828256ba570832abd9
Vincent Cubit <vinniecubit@yahoo.com>
2024-07-29 20:26:12 -0500

    Changed size of HealthCure image on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,16 @@ export default function Home({ searchParams }: SearchParamProps) {
 
       <section className="remove-scrollbar container m-auto">
         <div className="sub-container max-w-[496px]">
-          <Image
-            src="/assets/icons/logo-full.svg"
-            height={1000}
-            width={1000}
-            alt="patient"
-            className="mb-12 h-10 w-fit"
-          />
+          
+          <div className="w-1/2 mb-10">
+            <Image
+              src="/assets/icons/logo-full.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              layout="responsive"
+            />
+          </div>
 
           <PatientForm />
 
